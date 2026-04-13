f73fc04c0892cab2b97871ad988a4e9e6eb122d4
Ellie Norman <eleanornorman9@gmail.com>
2026-04-13 15:27:50 +0100

    Band Name Generator Task

---
+++ script.py
@@ -0,0 +1,4 @@
+print ("Welcome to the Band Name Generator.")
+name_of_town= input("What's the name of the town you grew up in?")
+name_of_pet= input("What's your pet's name?")
+print("Your band name could be" + name_of_town + " " + name_of_pet)
